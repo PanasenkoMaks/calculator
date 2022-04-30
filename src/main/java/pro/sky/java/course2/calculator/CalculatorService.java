49a217cdeb2acf66f9df760b6f4736bb54fada82
PanasenkoMaks <panasenko_maks@mail.ru>
2022-04-30 21:56:44 +0300

    1

--- src/main/java/pro/sky/java/course2/calculator/CalculatorService.java
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorService.java
@@ -1,2 +1,31 @@
-package pro.sky.java.course2.calculator;public class CalculatorService {
+package pro.sky.java.course2.calculator;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class CalculatorService {
+    public String Hello (){
+        return "Hello";
+    }
+
+    public int calculatorSum (int num1, int num2){
+        int rezult = num1 + num2;
+        return rezult;
+    }
+
+    public int calculatorMinus (int num1, int num2){
+        int rezult = num1 - num2;
+        return rezult;
+    }
+    public int calculatorMultiply (int num1, int num2){
+        int rezult = num1 * num2;
+        return rezult;
+    }
+
+    public int calculatorDivide (int num1, int num2){
+        int rezult = num1 / num2;
+        return rezult;
+    }
 }
